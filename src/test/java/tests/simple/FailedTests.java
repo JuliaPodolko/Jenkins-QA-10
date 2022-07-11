ca734f9e8bc30a26642b13bfe1b0b61b059f1b30
Julia Podolko <Julia Podolkoemail>
2022-07-11 18:05:06 +0500

    renamed NegativeTests to FailedTests

--- src/test/java/tests/simple/FailedTests.java
+++ src/test/java/tests/simple/FailedTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class NegativeTests {
+public class FailedTests {
     @Test
     void test00() {
         assertTrue(false);
